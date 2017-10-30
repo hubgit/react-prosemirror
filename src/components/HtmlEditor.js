112dafba0585bef2e6baab24133d18e58db7c6d7
Alf Eaton <eaton.alf@gmail.com>
2017-10-30 12:36:54 +0000

    Use 'lodash'

--- src/components/HtmlEditor.js
+++ src/components/HtmlEditor.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import debounce from 'lodash.debounce'
+import debounce from 'lodash/debounce'
 import { DOMParser, DOMSerializer } from 'prosemirror-model'
 
 import Editor from './Editor'
