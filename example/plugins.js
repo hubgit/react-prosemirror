9e015ffd68f051b0ee662f874a6dc132ee41205b
Alf Eaton <eaton.alf@gmail.com>
2017-11-01 13:42:41 +0000

    Move menubar into example

--- example/plugins.js
+++ example/plugins.js
@@ -3,6 +3,7 @@ import { Plugin } from 'prosemirror-state'
 import { dropCursor } from 'prosemirror-dropcursor'
 import { gapCursor } from 'prosemirror-gapcursor'
 import { columnResizing, tableEditing } from 'prosemirror-tables'
+import { MenuBarView } from './menubar'
 
 import menu from './menu'
 import keys from './keys'
@@ -11,12 +12,14 @@ import rules from './rules'
 export default [
   rules,
   keys,
-  menu,
   dropCursor(),
   gapCursor(),
   history(),
   columnResizing(),
   tableEditing(),
+  new Plugin({
+    view: editorView => new MenuBarView(editorView, menu)
+  }),
   new Plugin({
     props: {
       attributes: { class: 'prosemirror-editor' }
