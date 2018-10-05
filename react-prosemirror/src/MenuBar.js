1e27c0790b02d89ab79090601795ca54388a9ba5
Stuart Runyan <shrunyan@gmail.com>
2018-10-05 13:38:46 -0700

    added: logic for determine floating menu position

--- react-prosemirror/src/MenuBar.js
+++ react-prosemirror/src/MenuBar.js
@@ -22,23 +22,50 @@ const Button = (state, dispatch) => (item, key) => (
   </button>
 );
 
-const MenuBar = ({
-  menu,
-  children,
-  state,
-  dispatch,
-  className,
-  style
-}) => (
-  <div className={classnames(classes.bar, className)} style={style}>
-    {children && <span className={classes.group}>{children}</span>}
+class MenuBar extends React.Component {
+  render () {
+    let {
+      menu,
+      children,
+      state,
+      dispatch,
+      view,
+      floating,
+      className,
+      styles
+    } = this.props
 
-    {map(menu, (item, key) => (
-      <span key={key} className={classes.group}>
-        {map(item, Button(state, dispatch))}
-      </span>
-    ))}
-  </div>
-);
+    if (floating) {
+      styles = { ...styles };
+      if (view) {
+        if (state.selection.$anchor.pos !== state.selection.$head.pos) {
+          const coords = view.coordsAtPos(state.selection.$anchor.pos);
+
+          styles.height = "auto";
+          styles.padding = "3px";
+          // Avoid rendering menu out of window when cursor is at the top
+          styles.top = (coords.top - 40 > 0 ? coords.top - 40 : coords.top + 20);
+          // Avoid rendering menu past right window
+          styles.left = ((window.innerWidth - this.menu.offsetWidth) < coords.left) ? (coords.left - this.menu.offsetWidth) + 20 : coords.left;
+        }
+      }
+    }
+    return (
+      <div
+        ref={node => this.menu = node}
+        className={classnames(classes.bar, floating ? classes.floating : '', className)}
+        style={styles}
+      >
+        {children && <span className={classes.group}>{children}</span>}
+
+        {map(menu, (item, key) => (
+          <span key={key} className={classes.group}>
+            {map(item, Button(state, dispatch))}
+          </span>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default MenuBar;
