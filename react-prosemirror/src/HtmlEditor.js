b5e391c851421df3c5891f8085c4130183f10fb2
Sebastian Meier <s.meier@schuchert.de>
2020-05-22 10:28:41 +0200

    move parse and serialize functions to class and update editor state on value update

--- react-prosemirror/src/HtmlEditor.js
+++ react-prosemirror/src/HtmlEditor.js
@@ -4,35 +4,49 @@ import { DOMParser, DOMSerializer } from 'prosemirror-model'
 
 import Editor from './Editor'
 
-const parser = schema => {
-  const parser = DOMParser.fromSchema(schema)
-
-  return content => {
-    const container = document.createElement('article')
-    container.innerHTML = content
-
-    return parser.parse(container)
-  }
-}
-
-const serializer = schema => {
-  const serializer = DOMSerializer.fromSchema(schema)
-
-  return doc => {
-    const container = document.createElement('article')
-    container.appendChild(serializer.serializeFragment(doc.content))
-
-    return container.innerHTML
-  }
-}
-
 class HtmlEditor extends React.Component {
+
+  constructor (props) {
+    super(props)
+
+    this.editorRef = React.createRef()
+  }
+
+  get view() {
+    if (!this.editorRef || !this.editorRef.current) {
+      return null;
+    }
+    return this.editorRef.current.view;
+  }
+
+  initializeParse(schema) {
+    const parser = DOMParser.fromSchema(schema)
+  
+    return content => {
+      const container = document.createElement('article')
+      container.innerHTML = content
+  
+      return parser.parse(container)
+    }
+  }
+  
+  initializeSerialize(schema) {
+    const serializer = DOMSerializer.fromSchema(schema)
+  
+    return doc => {
+      const container = document.createElement('article')
+      container.appendChild(serializer.serializeFragment(doc.content))
+  
+      return container.innerHTML
+    }
+  }
+
   componentWillMount () {
     const { value, onChange, options } = this.props
     const { schema } = options
 
-    const parse = parser(schema)
-    const serialize = serializer(schema)
+    const parse = this.initializeParse(schema)
+    const serialize = this.initializeSerialize(schema)
 
     options.doc = parse(value)
 
@@ -41,11 +55,23 @@ class HtmlEditor extends React.Component {
     }, 1000, { maxWait: 5000 })
   }
 
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    const { value, options } = this.props
+    const { schema } = options
+
+    if (nextProps.value !== value) {
+      const parse = this.initializeParse(schema)
+      this.view.state.doc = parse(value)
+      this.view.updateState(this.view.state);
+    }
+  }
+
   render () {
     const { autoFocus, options, attributes, render, nodeViews } = this.props
 
     return (
       <Editor
+        ref={this.editorRef}
         autoFocus={autoFocus}
         options={options}
         attributes={attributes}
